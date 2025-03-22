924ea6d72831e6f4dfacac5dec4ba0aa725388ac
João Pedro <141186203+ByJoao1@users.noreply.github.com>
2025-03-22 16:51:57 -0300

    Add files via upload
    Esse é um teste feito por mim, usando o vs code.

---
+++ Teste aleatorio/1.py
@@ -0,0 +1,10 @@
+nome = float(input("Digite seu nome: "))
+
+if nome == "Chris":
+    print("Todo mundo odeia o Chris")
+elif nome == "Bruno":
+    print("Bruno é o melhor")
+else:
+
+    print (nome)
+
